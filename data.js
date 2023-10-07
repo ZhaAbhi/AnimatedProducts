a8b39acd7a6581dc3bdce927b10a8f3d54e55c4b
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-10-07 13:49:03 +0545

    Final animation

---
+++ data.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    type: 'Submariner',
+    imageUri: require('./assets/WatchCollection/rolex1.png'),
+    heading: 'Vibrant color',
+    description: "The supreme diver's watch",
+    key: 'first',
+    color: '#a1eeff',
+  },
+  {
+    type: 'GMT-Master ||',
+    imageUri: require('./assets/WatchCollection/rolex2.png'),
+    heading: 'Exceptional',
+    description: 'Two time-zones, uniquely iconic',
+    key: 'second',
+    color: '#a37739',
+  },
+  {
+    type: 'Air-King',
+    imageUri: require('./assets/WatchCollection/rolex3.png'),
+    heading: 'Freedom spirit',
+    description: 'An ever-improved homage to the pioneers of the skies',
+    key: 'third',
+    color: '#81bfcc',
+  },
+  {
+    type: 'Oyster Perpetual',
+    imageUri: require('./assets/WatchCollection/rolex4.png'),
+    heading: 'Quintessentially',
+    description: 'Make the world your oyster',
+    key: 'fourth',
+    color: '#e3d2ac',
+  },
+];
